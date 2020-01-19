47d4175f971f57fd4165ff4904b92db4ab053577
HYEOK999 <iasg2004@naver.com>
2020-01-19 22:50:29 +0900

    feat : Move the <Link> tag in App.js to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = props => {
-  return <div>Home</div>;
+  return (
+    <ul>
+      <li>
+        <Link to="/">홈</Link>
+      </li>
+      <li>
+        <Link to="/signin">로그인</Link>
+      </li>
+    </ul>
+  );
 };
 
 export default Home;
