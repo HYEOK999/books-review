72a3417d51103526eb6c6f6e1299f5588ecdbc5b
HYEOK999 <iasg2004@naver.com>
2020-01-19 22:52:47 +0900

    feat : Update the SigninBg
    1. Use the antd { Col }
    2. Use the styled-components

--- src/components/Signin/SigninBg.jsx
+++ src/components/Signin/SigninBg.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Col } from 'antd';
+import styled from 'styled-components';
 
-const SigninBg = props => {
-  return <div>SigninBg</div>;
-};
+const StyledCol = styled(Col).attrs(() => ({
+  span: 12,
+}))``;
+
+const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const SigninBg = () => (
+  <StyledCol>
+    <StyledImg src="/book.jpg" alt="Signin" />
+  </StyledCol>
+);
 
 export default SigninBg;
