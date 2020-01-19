16d0c4bbea13fb8015d15d12d027282818a340fe
HYEOK999 <iasg2004@naver.com>
2020-01-19 23:09:55 +0900

    feat : Add the StyledLoginTitle

--- src/components/Signin/SigninForm/SigninForm.jsx
+++ src/components/Signin/SigninForm/SigninForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Col } from 'antd';
 import styled from 'styled-components';
+import SigninLoginForm from './SigninLoginForm';
+import SigninSupportUl from './SigninSupportUl';
 
 const StyledCol = styled(Col).attrs(() => ({
   span: 12,
@@ -8,6 +10,19 @@ const StyledCol = styled(Col).attrs(() => ({
   vertical-align: top;
 `;
 
-const SigninForm = () => <StyledCol>test</StyledCol>;
+const StyledLoginTitle = styled.h2`
+  text-align: center;
+  padding: 50px 0 30px 0px;
+  margin: 0;
+  font-weight: inherit;
+`;
+
+const SigninForm = () => (
+  <StyledCol>
+    <StyledLoginTitle>LOG IN. START SEARCHING.</StyledLoginTitle>
+    <SigninLoginForm></SigninLoginForm>
+    <SigninSupportUl></SigninSupportUl>
+  </StyledCol>
+);
 
 export default SigninForm;
