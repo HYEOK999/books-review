97c718beb399137862e82c258e3ca3a32228bb07
HYEOK999 <iasg2004@naver.com>
2020-01-19 23:36:49 +0900

    feat : Add Components to SigninLoginForm
    1. Add the MarkUp
    2. Add the Styled Components

--- src/components/Signin/SigninForm/SigninLoginForm.jsx
+++ src/components/Signin/SigninForm/SigninLoginForm.jsx
@@ -1,7 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const SigninLoginForm = props => {
-  return <div>test</div>;
+const SigninLoginForm = ({ className }) => {
+  return (
+    <form className={className}>
+      <fieldset>
+        <div>
+          <label>Email</label>
+          <input autoFocus type="email" placeholder="Enter your E-mail" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" placeholder="Enter your Password" />
+        </div>
+        <button>SIGN IN</button>
+      </fieldset>
+    </form>
+  );
 };
 
-export default SigninLoginForm;
+const StyledLoginForm = styled(SigninLoginForm)`
+  padding: 50px 0 25px 0;
+  label,
+  input,
+  button {
+    font-weight: inherit;
+  }
+  label::after {
+    content: '*';
+    color: red;
+  }
+  input {
+    margin: 10px 0 20px;
+    display: block;
+    width: 100%;
+    height: 30px;
+    background-color: rgb(227, 238, 253);
+    border: rgba(117, 117, 117, 0.2) solid 1px;
+  }
+
+  input::placeholder {
+    font-style: italic;
+    color: rgba(19, 19, 19, 0.3);
+    font-weight: 600;
+    padding-left: 10px;
+  }
+
+  input:focus {
+    background-color: #fff;
+    outline: none !important;
+    border-color: #719ece;
+    box-shadow: 0 0 10px #719ece;
+  }
+
+  button {
+    background-color: rgb(30, 74, 94);
+    border: none;
+    color: rgb(241, 246, 247);
+    font-size: 16px;
+    padding: 6px 30px;
+  }
+
+  button:hover {
+    color: rgb(30, 74, 94);
+    background-color: rgb(241, 246, 247);
+    box-shadow: 1px 1px rgb(30, 74, 94), -1px -1px rgb(30, 74, 94),
+      1px -1px rgb(30, 74, 94), -1px 1px rgb(30, 74, 94);
+  }
+`;
+
+export default StyledLoginForm;
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import styled from 'styled-components';
 import { Row, Col } from 'antd';
 import SigninBg from '../components/Signin/SigninBg';
