1ce49bbff8d350d9579892fb86998226ac850eaf
HYEOK999 <iasg2004@naver.com>
2020-01-19 23:38:53 +0900

    feat : Add Components to SigninSupportUl
    1. Add the MarkUp
    2. Add the Styled Components

--- src/components/Signin/SigninForm/SigninSupportUl.jsx
+++ src/components/Signin/SigninForm/SigninSupportUl.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const SigninSupportUl = props => {
-  return <div>test</div>;
+const SigninSupportUl = ({ className }) => {
+  return (
+    <ul className={className}>
+      <li>
+        <span>Need to create an account?</span>
+        <button>SIGN UP</button>
+      </li>
+      <li>
+        <span>Forgot your password</span>
+        <button>RECOVERY</button>
+      </li>
+    </ul>
+  );
 };
 
-export default SigninSupportUl;
+const StyledSupportUl = styled(SigninSupportUl)`
+  list-style: none;
+  padding: 25px 0 0;
+  margin: 0;
+  border-top: rgba(233, 233, 233) solid 1px;
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  li:nth-child(2) {
+    margin-top: 25px;
+  }
+  span {
+    margin: 0;
+    margin-bottom: 0;
+    font-size: 15px;
+  }
+  button {
+    background-color: inherit;
+    padding: 5px 20px;
+    border: rgb(29, 74, 93) 2px solid;
+    color: rgb(71, 107, 123);
+    font-weight: 600;
+  }
+
+  button:hover {
+    color: rgb(241, 246, 247);
+    background-color: rgb(29, 74, 93);
+  }
+`;
+
+export default StyledSupportUl;
