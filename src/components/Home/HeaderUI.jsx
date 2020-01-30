a5ed06e6ff863211fcba8cd51f991c4276ea1258
HYEOK999 <iasg2004@naver.com>
2020-01-30 21:08:29 +0900

    feat : Change the CSS

--- src/components/Home/HeaderUI.jsx
+++ src/components/Home/HeaderUI.jsx
@@ -11,6 +11,19 @@ const StyledNavMenu = styled(Menu).attrs(() => ({
   line-height: 64px;
 `;
 
+const StyledLogoH1 = styled.h1`
+  position: absolute;
+  left: 30px;
+  display: inline-block;
+  background: url(/BookLogoImg.png) no-repeat;
+  background-size: contain;
+  height: 64px;
+  width: 100px;
+  text-indent: 100px;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
 const StyledLogoutButton = styled.button`
   position: absolute;
   top: -6px;
@@ -31,12 +44,14 @@ const StyledLogoutButton = styled.button`
     top: 22px;
     right: 4px;
     font-size: 12px;
+    height: 45px;
   }
 `;
 
 const HeaderUI = ({ logout }) => {
   return (
-    <Header className="header">
+    <Header style={{ padding: '0 122px' }} className="header">
+      <StyledLogoH1>Books</StyledLogoH1>
       <StyledLogoutButton onClick={logout}>
         <Icon type="logout" />
       </StyledLogoutButton>
