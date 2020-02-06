d1c262abe9dd8be43f79c6b799c322b58156dc08
HYEOK999 <iasg2004@naver.com>
2020-02-06 14:39:34 +0900

    feat : Add the reducer

--- src/reducer/loading.js
+++ src/reducer/loading.js
@@ -1,12 +1,12 @@
 import { START_LOADING, END_LOADING } from '../actions';
 
-const initialState = false;
+const initialState = null;
 
 const loading = (state = initialState, action) => {
   if (action.type === START_LOADING) {
     return true;
   } else if (action.type === END_LOADING) {
-    return false;
+    return null;
   }
   return state;
 };
